e4496f8c6d426df6520e4955b1ffac7508bb0623
Stn77 <fudinyusuf20@gmail.com>
2026-01-08 12:32:12 +0700

    gwendeng gwendeng part 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,6 +52,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@smkprima.com') && $this->hasVerifiedEmail();
+        return true;
     }
 }
